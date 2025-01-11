40ebba014d017acbf52aa1bd36211abeb015fac9
Takap <yyyymmddhhmmsssss@gmail.com>
2025-01-11 16:57:47 +0900

    init

---
+++ FileUtility.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CombineFiles
+{
+    public partial class FileUtility
+    {
+        /// <summary>
+        /// 指定したディレクトリ内にあるファイルの全ての拡張子を取得する。
+        /// </summary>
+        public static IEnumerable<string> GetExtensions(string rootDir)
+        {
+            Dictionary<string, string> tmpTable = new();
+            foreach (var path in Directory.GetFileSystemEntries(rootDir, "*.*", SearchOption.AllDirectories))
+            {
+                string extension = Path.GetExtension(path).ToLower();
+                if (!tmpTable.ContainsKey(extension))
+                {
+                    tmpTable[extension] = extension;
+                }
+            }
+            return tmpTable.Keys;
+        }
+
+        /// <summary>
+        /// 指定したフォルダパス以下の対象ファイルを全て一つにまとめます。
+        /// </summary>
+        public static void PackFiles(string rootDir, string extensions, string ignores, string outFilePath)
+        {
+            extensions = extensions.ToLower(); // 全て小文字で判定する
+            string[] extArray = extensions.Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+            ignores = ignores.ToLower();
+            string[] ignoreArray = ignores.Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+            StreamWriter sw = null;
+
+            DirectoryInfo di = new(rootDir);
+            foreach (var path in Directory.GetFileSystemEntries(rootDir, "*.*", SearchOption.AllDirectories))
+            {
+                string fileExt = Path.GetExtension(path).ToLower();
+                bool isHit = Array.Exists(extArray, ext => ext == fileExt); // 拡張子でフィルター
+                if (!isHit)
+                {
+                    continue;
+                }
+                FileInfo fi = new(path);
+                string relatibveFilePath = fi.FullName[(di.FullName.Length + 1)..];
+
+                string lr = relatibveFilePath.ToLower();
+                bool isIgnore = Array.Exists(ignoreArray, lr.Contains); // 除外パターンでフィルター
+                if (isIgnore)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(relatibveFilePath);
+
+                // 出力先のファイルに書き込み
+                sw ??= new StreamWriter(outFilePath, false);
+                sw.WriteLine("```");
+                sw.WriteLine("// " + relatibveFilePath);
+                sw.WriteLine();
+
+                // 結果が文字化けしないようにエンコードを簡易判定する
+                Encoding enc = EncodeUtil.GetEncoding(File.ReadAllBytes(path));
+                enc ??= Encoding.UTF8;
+
+                foreach (var line in File.ReadAllLines(path, enc))
+                {
+                    string tmpLine = line.Trim(); // 行頭・行末のスペースなどを除去(AIが読むときは必要ない
+                    if (tmpLine == "")
+                    {
+                        continue; // 空行をまとめない
+                    }
+
+                    // 複数連続するスペースとタブを一つにまとめる
+                    string p = Reg1().Replace(tmpLine, " ");
+                    p = Reg2().Replace(p, " ");
+
+                    sw.WriteLine(p);
+                }
+
+                sw.WriteLine("```");
+                sw.WriteLine();
+            }
+
+            sw?.Dispose();
+            string body = File.ReadAllText(outFilePath);
+            Console.WriteLine($"Length={body.Length}");
+        }
+
+        [GeneratedRegex(@"\s+")]
+        private static partial Regex Reg1();
+        [GeneratedRegex(@"\t+")]
+        private static partial Regex Reg2();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using System.Diagnostics;
+
+namespace CombineFiles
+{
+    // 
+    // 説明:
+    // コードを一つのファイルにまとめる
+    // Geminiが200万文字まで認識してくれることを期待して特定のフォルダ内にある全てのソースコードを1つのファイルにまとめるツール
+    // 
+    // Args:
+    // [0]: 動作モード, 0: 拡張子を列挙する(args[2]に指定するため) / 1: 指定したファイルにまとめる
+    // [1]: 検索対象のフォルダパス
+    // [2]: 抽出対象の拡張子リスト(動作モード1のときは必須)
+    //      指定例: ".cpp;.h;.hpp" (セミコロン区切りでドットが必要)
+    // [3]: 無視パターン(このパターンが含まれるパスは出力対象にしない)
+    //      指定例: "ignoreDirName1;ignoreDirName2" (セミコロン区切り)
+    // 
+    // 使用例:
+    // (1)
+    // 対象フォルダにどんな拡張子が含まれているのか調べる例
+    // 出力をクリップボードにコピーする例
+    // xxx.exe 0 c:\xxx\xxx | clip
+    //
+    // (2)
+    // 指定したフォルダ内のファイルを1つにまとめる
+    // xxx.exe 1 c:\workspace\sample .cpp;.h;.hpp ignoreDirName1;ignoreDirName2
+    // 
+
+    internal class AppMain
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 2)
+                {
+                    throw new Exception("動作モードと対象フォルダパスを指定してください。");
+                }
+                if (!TryGetMode(args[0], out Mode mode))
+                {
+                    throw new Exception("動作モードを指定してください。args[0]");
+                }
+                if (!Directory.Exists(args[1]))
+                {
+                    throw new Exception($"ディレクトリではありません。arg[1]={args[1]}");
+                }
+                string targetDir = args[1];
+
+                switch (mode)
+                {
+                    case Mode.ListExtensions:
+                    {
+                        string allExtensions = string.Join(';', FileUtility.GetExtensions(targetDir));
+                        Console.Write(allExtensions);
+                        break;
+                    }
+                    case Mode.PackFiles:
+                    {
+                        Console.WriteLine("Pack");
+
+                        if (args.Length < 3)
+                        {
+                            throw new Exception("拡張子リストがありません");
+                        }
+                        string extensions = args[2];
+                        string ignores = args[3];
+
+                        // 出力先のファイルパス
+                        DirectoryInfo di = new(targetDir);
+                        string outPath = Path.Combine(di.Parent.FullName, $"{DateTime.Now:yyyyMMddHHmmss}.txt");
+                        //string outPath = @"c:\temp\temp.txt";
+
+                        FileUtility.PackFiles(targetDir, extensions, ignores, outPath);
+                        Process.Start("explorer", $"/select,\"{outPath}\""); // 選択状態でエクスプローラーを開く
+                        break;
+                    }
+                    default:
+                    {
+                        throw new Exception($"動作モードを指定してください。value={args[2]}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"エラーが発生しました。Message=" + ex.Message);
+                DumpHelp();
+            }
+        }
+
+        /// <summary>
+        /// 指定した文字列から動作モードを取得します。
+        /// </summary>
+        static bool TryGetMode(string value, out Mode mode)
+        {
+            if (int.TryParse(value, out int result))
+            {
+                if (result == 0)
+                {
+                    mode = Mode.ListExtensions;
+                    return true;
+                }
+                else if (result == 1)
+                {
+                    mode = Mode.PackFiles;
+                    return true;
+                }
+            }
+            mode = Mode.Default;
+            return false;
+        }
+
+        static void DumpHelp()
+        {
+            Console.WriteLine("説明:");
+            Console.WriteLine("コードを一つのファイルにまとめる");
+            Console.WriteLine("Geminiが200万文字まで認識してくれることを期待して特定のフォルダ内にある全てのソースコードを1つのファイルにまとめるツール");
+            Console.WriteLine("");
+            Console.WriteLine("Args:");
+            Console.WriteLine("[0]: 動作モード, 0: 拡張子を列挙する(args[2]に指定するため) / 1: 指定したファイルにまとめる");
+            Console.WriteLine("[1]: 検索対象のフォルダパス");
+            Console.WriteLine("[2]: 抽出対象の拡張子リスト(動作モード1のときは必須)");
+            Console.WriteLine("     指定例: \".cpp;.h;.hpp\" (セミコロン区切りでドットが必要)");
+            Console.WriteLine("[3]: 無視パターン(このパターンが含まれるパスは出力対象にしない)");
+            Console.WriteLine("     指定例: \"OSS;web_client\" (セミコロン区切り)");
+            Console.WriteLine("");
+            Console.WriteLine("使用例:");
+            Console.WriteLine("(1)");
+            Console.WriteLine("対象フォルダにどんな拡張子が含まれているのか調べる例");
+            Console.WriteLine("出力クリップボードにコピーする例");
+            Console.WriteLine("xxx.exe 0 c:\\xxx\\xxx | clip");
+            Console.WriteLine("");
+            Console.WriteLine("(2)");
+            Console.WriteLine("指定したフォルダ内のファイルを1つにまとめる");
+            Console.WriteLine("xxx.exe 1 c:\\workspace\\sample .cpp;.h;.hpp OSS;web_client");
+        }
+
+        /// <summary>
+        /// 動作モードを表します。
+        /// </summary>
+        enum Mode
+        {
+            /// <summary>初期値</summary>
+            Default = 0,
+            /// <summary>拡張子を列挙</summary>
+            ListExtensions,
+            /// <summary>ファイルをまとめる</summary>
+            PackFiles,
+        }
+    }
+}
